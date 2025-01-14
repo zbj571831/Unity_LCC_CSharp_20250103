46721033d269e2c1253e343200996aab943319bf
abcd1824 <zbj571831@gmail.com>
2025-01-14 22:43:53 +0800

    Update完成作業練習

---
+++ Assets/Scripts/Exercise_1_Variable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Exercise_1_Variable : MonoBehaviour
+{
+    public int price = 999;
+    private float Jumpheight = 77.5f;
+    public string arms = "三項之力";
+    private bool DragonSoul = false;
+}
