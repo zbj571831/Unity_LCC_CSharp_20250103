4e915f3bf3fdf659426f3a204c72177e043d338a
abcd1824 <zbj571831@gmail.com>
2025-01-19 20:30:53 +0800

    呼叫方法與回傳

--- Assets/Scripts/Class_4_Function.cs
+++ Assets/Scripts/Class_4_Function.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 /// <summary>
 /// 方法
 /// 中文 : 方法 `函式ˋ函數，功能
@@ -8,6 +9,8 @@ public class Class_4_Function : MonoBehaviour
 {
     private void Awake()
     {
+        #region 方法基本語法與參數呼叫練習
+
         //呼叫方法 : 方法沒有呼叫不會執行
         //方法名稱();
         FirstFunction();
@@ -15,9 +18,37 @@ public class Class_4_Function : MonoBehaviour
         FirstFunction();
         //呼叫有參數的方法 : 小括號內要放入引數
         //UseSkill();  //錯誤 : 少引數
-        UseSkill("火球術");
-        UseSkill("雷擊術");
+        UseSkill("火球術", 30);
+        UseSkill("雷擊術", 50);
+        //呼叫預設值參數的方法 : 可以不用填寫 (選填式參數)
+        SpawnEnemy();
+        SpawnEnemy("石頭人");
+
+        Fire(700);
+        Fire(800, "保齡球");
+        //有多個選擇性參數時的呼叫
+        //買紅水50
+        BuyItem();
+        //BuyItem(30); 錯誤 :30 會直接帶入到 item 參數導致資料類型不符合
+        BuyItem(count: 30);
+        //購買藍水50
+        BuyItem("藍水");
+        //購買藍水100
+        BuyItem("藍水", 100);
+
+
+        #endregion
+
+        //呼叫有傳回方法
+        //第一種 : 把結果放到區域變數內
+        int number9 = Square(9);
+        Debug.Log($"<color=#a33>9 的平方{number9}</color>");
+        //第二種 : 把傳回方法當作該傳回類型使用
+        Debug.Log($"<color=#a33>7 的平方{Square(7)}</color>");
     }
+
+    //框選要整理的程式 > ctrl + K S > #region 快速完成程式碼片段
+    #region 方法與基本語法與參數
     //方法 :
     //包含一系列程式區塊
     //方法語法 :
@@ -29,8 +60,55 @@ public class Class_4_Function : MonoBehaviour
         Debug.Log("第一個方法");
     }
     // 參數語法 : 與區域變數相同 資料類型 參數名稱 (習慣用小寫或者加底線開頭)
-    private void UseSkill(string skill)
+    private void UseSkill(string skill, float cost)
     {
         Debug.Log($"<color=#f93>施放技能 : {skill}</color>");
+        Debug.Log($"<color=#f93>技能消耗 : {cost}</color>");
     }
+
+    private void SpawnEnemy(string enemy = "哥布林")
+    {
+        Debug.Log($"<color=#f33>生成敵人: {enemy}</color>");
+
+    }
+    //沒有預設值稱為 : 必要參數，有預設值稱為選填式參數
+    //選填式參數要寫在右邊，下方是錯誤範例
+    //private void Fire(string fire = "子彈", int speed)
+    //{
+    //    Debug.Log($"<color=#66f>發射物件 : {fire},速度 : {speed}</color>");
+    //}
+
+    /// <summary>
+    /// 發射物件方法
+    /// </summary>
+    /// <param name="speed">發射物件</param>
+    /// <param name="fire">要發射物件速度]</param>
+    private void Fire(int speed, string fire = "子彈")
+    {
+        Debug.Log($"<color=#66f>發射物件 : {fire},速度 : {speed}</color>");
+    }
+
+    /// <summary>
+    /// 購買道具
+    /// </summary>
+    /// <param name="item">道具名稱</param>
+    /// <param name="count">數量</param>
+    private void BuyItem(string item = "紅水", int count = 50)
+    {
+        Debug.Log($"<color=#3f3>購買商品 : {item},數量 : {count}</color>");
+    }
+
+    #endregion
+    //傳回方法 : 傳回類型不是 void，呼叫該方法會獲得結果
+    //傳回方法必須在 {} 內使用 return 關鍵字將結果傳回
+    /// <summary>
+    /// 平方
+    /// </summary>
+    /// <param name="number">要的平方數字</param>
+    /// <returns>數字的平方</returns>
+    private int Square(int number)
+    {
+        return number * number ;
+    }
+
 }
