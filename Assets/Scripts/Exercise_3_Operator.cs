44f3d53901a8da35eb7b3e9376b954ed4583fba4
abcd1824 <zbj571831@gmail.com>
2025-01-19 21:51:32 +0800

    完成作業練習

---
+++ Assets/Scripts/Exercise_3_Operator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Exercise_3_Operator : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log(6/2*(1+2));
+    }
+}
---
+++ Assets/Scripts/Exercise_4_Function.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Exercise_4_Function : MonoBehaviour
+{
+    private void Awake()
+    {
+        bool a = kk(true);
+        Debug.Log($"{a}");
+
+        Debug.Log($"{boo()}");
+    }
+    private bool  kk( bool a = true)
+    {
+        return  a;
+    }
+    private bool boo(bool a=true, bool b = false)
+    {
+        return a && b;
+    }
+}
