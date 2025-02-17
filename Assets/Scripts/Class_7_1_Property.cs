691adf64e7912f336dc0e0b3b1cd438ceb5f304e
abcd1824 <zbj571831@gmail.com>
2025-02-17 17:48:58 +0800

    認識屬性

---
+++ Assets/Scripts/Class_7_1_Property.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+namespace jeff 
+{
+    /// <summary>
+    /// 屬性 Property
+    /// </summary>
+    public class Class_7_1_Property : MonoBehaviour
+    {
+        #region 認識屬性 Property
+        //公開的變數 : 可以讓外部讀取與寫入
+        public float mobveSpeed = 3.5f;
+
+        //私人的變數 : 不允許外部讀取與寫入
+        private float turnSpeed = 12.7f;
+
+        //公開的屬性 : 允許存取
+        public float runSpeed { get; set; }
+        //私人的屬性 : 不允許存取
+        private float sprintSpeed { get; set; }
+        //公開的屬性 : 只有 get 代表唯讀 (只能讀取不能寫入)
+        public float jumpSpeed { get; }
+
+        //prop + Tab
+        //有預設值的屬性
+        
+        public byte lv { get; set; } = 1;
+
+        private void Awake()
+        {
+            Debug.Log("-----取得變數與屬性-----");
+            Debug.Log(mobveSpeed);
+            Debug.Log(turnSpeed);
+            Debug.Log(runSpeed);
+            Debug.Log(sprintSpeed);
+            Debug.Log(jumpSpeed);
+            Debug.Log(lv);
+            Debug.Log("-----設定變數與屬性-----");
+            mobveSpeed = 11.1f;
+            turnSpeed = 2.5f;           //私人變數在類別內可以設定
+            runSpeed = 30;
+            sprintSpeed = 60.2f;
+            //jumpSpeed = 90;          //沒有 set 的屬性在類別內也無法設定
+            lv = 10;
+        }
+        #endregion
+
+        //屬性簡寫
+        public float hp { get; set; } = 100;
+
+        //屬性標準寫法
+        //_mp 用裝屬性 mp 值得容器
+        private float _mp = 50;
+        //屬性給予外部存取權限的窗口
+        private float mp
+        {
+            get
+            {
+                return _mp;
+            }
+
+            set
+            {
+                _mp = value;
+            }
+        }
+
+        //取得屬性的簡寫 (箭頭 => Lambda)
+        //唯獨屬性 防禦值是30
+        public int defence { get; } = 30;
+        //唯獨屬性 攻擊值是50
+        public int attack => 50;
+
+        private void Start()
+        {
+            Debug.Log($"<color=#77f>魔力 :{mp}</color>");
+            mp = 500;
+            Debug.Log($"<color=#77f>魔力 :{mp}</color>");
+        }
+    
+    }
+}
+
