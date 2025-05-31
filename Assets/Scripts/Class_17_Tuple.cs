ca721714ff195f415d07e4d5534672ed56c8ce04
abcd1824 <zbj571831@gmail.com>
2025-05-31 22:09:40 +0800

    元祖與例外處理

---
+++ Assets/Scripts/Class_17_Tuple.cs
@@ -0,0 +1,57 @@
+﻿using jeff.Tools;
+using UnityEngine;
+
+namespace jeff
+{
+    public class Class_17_Tuple : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // 元祖 : 儲存多筆資料不同類型的資料
+            // 宣告方法 1 : 指定類型與名稱
+            (string name, int cost, int index) card1 = ("史萊姆", 1, 7);
+            LogSystem.LogWithColor($"{card1.name} | 消耗 : {card1.cost} | 編號 : {card1.index}", "#f49");
+
+            // 宣告方法 2 : 指定類型
+            // 取得元祖衣料時使用 Item1~ItemN
+            (string , int , int ) card2 = ("哥布林", 2, 23);
+            LogSystem.LogWithColor($"{card2.Item1} | 消耗 : {card2.Item2} | 編號 : {card2.Item3}", "#f49");
+
+            // 宣告方法 3 : 使用 var
+            var card3 = ("樹精", 4, 50);
+            LogSystem.LogWithColor($"{card3.Item1} | 消耗 : {card3.Item2} | 編號 : {card3.Item3}", "#f49");
+
+            //宣告方法 4 : 使用 var 以及 指名
+            var card4 = (name:"蝙蝠",cost: 2, index: 6);
+            LogSystem.LogWithColor($"{card4.Item1} | 消耗 : {card4.Item2} | 編號 : {card4.Item3}", "#f49");
+
+            UseCard(card1);
+            UseCard(card2);
+            UseCard(("暴龍", 7 ,199));
+
+            var cardlUpdate = UpdateCardCost(card1);
+            LogSystem.LogWithColor($"{cardlUpdate.name} | " +  $"消耗 : {cardlUpdate.cost} | 編號 : {cardlUpdate.index}", "#f16");
+
+            LogSystem.LogWithColor($"{cardlUpdate == card1}", "#f49");
+            LogSystem.LogWithColor($"{cardlUpdate != card1}", "#f49");
+        }
+
+        /// <summary>
+        /// 使用卡牌
+        /// </summary>
+        /// <param name="card">卡牌元祖，名稱，消耗與編號</param>
+        private void UseCard((string name, int cost, int index) card)
+        {
+            LogSystem.LogWithColor($"  消耗 : {card.cost} | 使用卡牌 : {card.name}", "#f49");
+        }
+
+        private (string name, int cost, int index) UpdateCardCost((string name, int cost, int index) card)
+        {
+            card.name = card.name + "降低消耗版本";
+            card.cost -= 1;
+            return card;
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Class_18_Exception.cs
@@ -0,0 +1,94 @@
+﻿using jeff.Tools;
+using System;
+using UnityEngine;
+
+namespace jeff
+{
+    /// <summary>
+    /// 例外處理
+    /// </summary>
+    public class Class_18_Exception : MonoBehaviour
+    {
+        private void Awake()
+        {
+            LogSystem.LogWithColor($"{Division(8, 4)}", "#f33");
+            LogSystem.LogWithColor($"{Division(3, 9)}", "#f33");
+            LogSystem.LogWithColor($"{Division(7, 0)}", "#f33");
+            //LogSystem.LogWithColor($"{Division(2, 9)}", "#f33");
+
+            LogSystem.LogWithColor($"{GetScores(0)}", "#f33");
+            LogSystem.LogWithColor($"{GetScores(4)}", "#f33");
+            LogSystem.LogWithColor($"{GetScores(9)}", "#f33");
+
+            SetEnemy();
+        }
+
+        /// <summary>
+        /// 除法
+        /// </summary>
+        /// <param name="numberA">分母</param>
+        /// <param name="numberB">分子</param>
+        /// int? 允許傳空值
+        private int? Division(int numberA, int numberB)
+        {
+            // 可能發生例外的區域
+            try
+            {
+                return numberA / numberB;    //發生例外
+            }
+            // 捕捉到例外為(除以零 ) 時會執行此區域
+            catch (DivideByZeroException e)
+            {
+                // Message 例外官方訊息
+                LogSystem.LogWithColor($"分子不能為零 | {e.Message}", "#f99");
+                return null;
+            }
+            // 最後區域
+            finally 
+            {
+                LogSystem.LogWithColor($"例外處理完畢", "#f73");
+            }
+            
+        }
+
+        private int[] scores = { 70, 91, 64, 53, 88 };
+
+        private int? GetScores(int index)
+        {
+            try
+            {
+                return scores[index];
+            }
+            catch (DivideByZeroException) 
+            {
+                LogSystem.LogWithColor($"發生例外", "#f11");
+                return null;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                LogSystem.LogWithColor($"發生例外 | {e.Message}", "#f11");
+                return null;
+            }
+
+        }
+
+        [SerializeField]
+        private GameObject enemy;
+
+        /// <summary>
+        ///  設定敵人物件
+        /// </summary>
+        private void SetEnemy()
+        {
+            try
+            {
+                enemy.SetActive(true);  // 顯示敵人物件
+            }
+            catch (Exception e)         // Exception 處理所有例外
+            {
+
+                LogSystem.LogWithColor($"發生例外 : {e.Message}", "#f39");
+            }
+        }
+    }
+}
